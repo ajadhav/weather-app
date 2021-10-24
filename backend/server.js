1aefd91e0759621bfbef08a5063f5a761c30d364
Anup <ajadhav@umich.edu>
2021-10-23 22:58:45 -0400

    fixing server

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 const api = require('./api');
 app.use('/api', api);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ message: err.message, error: err });
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.json({ message: err.message, error: err });
+// });
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
